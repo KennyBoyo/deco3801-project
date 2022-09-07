7b49135b5a6340b8f64810987b0eb13e23429aab
Eric Shen <eshriecn02@gmail.com>
2022-09-07 13:09:01 +1000

    Implemented GPT3 with mediawiki POST

--- test/apitesting.py
+++ test/apitesting.py
@@ -1,9 +1,10 @@
+
 import openai
 
 #dont leak this pls
 openai.api_key = "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"
 
-prompt = "Write a wikipedia article about the University of Queensland."
+prompt = "Write an article about the history of apples."
 
 output = openai.Completion.create(
     model="text-davinci-002",
@@ -12,6 +13,82 @@ output = openai.Completion.create(
     temperature=0.1
 )
 
-print(type(output["choices"][0]))
-print("Output:", output["choices"][0]["text"])
-print("Total Tokens:", output["usage"]["total_tokens"])
+text_output = output["choices"][0]["text"]
+
+#print(type(output["choices"][0]))
+#print("Output:", text_output)
+#print("Total Tokens:", output["usage"]["total_tokens"])
+
+
+
+"""
+TODO: implement openai response as the push request to mediawiki api using the POST function
+"""
+
+#!/usr/bin/python3
+
+"""
+    edit.py
+
+    MediaWiki API Demos
+    Demo of `Edit` module: POST request to edit a page
+    MIT license 
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://test.wikipedia.org/w/api.php"
+
+# Step 1: GET request to fetch login token
+PARAMS_0 = {
+    "action": "query",
+    "meta": "tokens",
+    "type": "login",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS_0)
+DATA = R.json()
+
+LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+# Step 2: POST request to log in. Use of main account for login is not
+# supported. Obtain credentials via Special:BotPasswords
+# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+PARAMS_1 = {
+    "action": "login",
+    "lgname": "Erkshen@bruh",
+    "lgpassword": "qinjqhiehsn9oi2554obl1or72cgnqca",
+    "lgtoken": LOGIN_TOKEN,
+    "format": "json"
+}
+
+R = S.post(URL, data=PARAMS_1)
+
+# Step 3: GET request to fetch CSRF token
+PARAMS_2 = {
+    "action": "query",
+    "meta": "tokens",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS_2)
+DATA = R.json()
+
+CSRF_TOKEN = DATA['query']['tokens']['csrftoken']
+
+# Step 4: POST request to edit a page
+PARAMS_3 = {
+    "action": "edit",
+    "title": "Apples",
+    "token": CSRF_TOKEN,
+    "format": "json",
+    "text": text_output
+}
+
+R = S.post(URL, data=PARAMS_3)
+DATA = R.json()
+
+print(DATA)
