d9d204fc9d88a21b072fd2d4a666793636316161
KennyBoyo <59184727+KennyBoyo@users.noreply.github.com>
2022-10-16 12:27:41 +1000

    updated update

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update bundle.js
-sudo cp ./src/scripts/update.sh /var/www/html/mediawiki
+sudo cp ./src/docker/bundle.js /var/www/html/mediawiki
 
 # update nginx config
 sudo cp ./src/scripts/mediawiki /etc/nginx/sites-available
