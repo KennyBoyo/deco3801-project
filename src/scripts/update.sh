9977c1b296114d38665b6b048397d017e55e3823
KennyBoyo <59184727+KennyBoyo@users.noreply.github.com>
2022-10-16 12:15:30 +1000

    updated web url

---
+++ src/scripts/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# update bundle.js
+sudo cp ./src/scripts/update.sh /var/www/html/mediawiki
+
+# update nginx config
+sudo cp ./src/scripts/mediawiki /etc/nginx/sites-available
+sudo ln -sf /etc/nginx/sites-available/mediawiki /etc/nginx/sites-enabled/mediawiki
+
+# update php config
+sudo cp ./src/scripts/php.ini /etc/php/7.4/fpm/php.ini
+
+# restart processes for update
+sudo systemctl restart php7.4-fpm.service
+sudo systemctl restart nginx
